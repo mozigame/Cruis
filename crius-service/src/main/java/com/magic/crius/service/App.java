7d93c98f4965b43fe636b402a76e1e7f6b6940e5
shawn <shawn@gmail.com>
2017-05-29 12:56:44 +0800

    add new version

---
+++ crius-service/src/main/java/com/magic/crius/service/App.java
@@ -0,0 +1,13 @@
+package com.magic.crius.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
