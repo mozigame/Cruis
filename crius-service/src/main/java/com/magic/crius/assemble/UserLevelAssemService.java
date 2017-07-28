b568dab2f226056ef1cb3ce23301297cf62c8f31
justin <justins1707@gmail.com>
2017-07-28 22:36:48 +0800

    log

--- crius-service/src/main/java/com/magic/crius/assemble/UserLevelAssemService.java
+++ crius-service/src/main/java/com/magic/crius/assemble/UserLevelAssemService.java
@@ -1,18 +1,25 @@
 package com.magic.crius.assemble;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import javax.annotation.Resource;
+
+import org.apache.log4j.Logger;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.magic.api.commons.tools.DateUtil;
+import com.magic.crius.enums.KafkaConf;
 import com.magic.crius.service.MemberConditionVoService;
 import com.magic.crius.service.UserInfoService;
 import com.magic.crius.vo.UserLevelReq;
 import com.magic.user.vo.MemberConditionVo;
-import org.apache.log4j.Logger;
-import org.springframework.stereotype.Service;
-
-import javax.annotation.Resource;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
 
 /**
  * User: joey
@@ -28,6 +35,14 @@ public class UserLevelAssemService {
     private MemberConditionVoService memberConditionVoService;
     @Resource
     private UserInfoService userInfoService;
+    
+    @Resource(name = "kafkaTemplate")
+    private KafkaTemplate<Integer, String> template;
+
+    private static final String TOPIC = "plutus";
+
+    private static final String DATA = "Data";
+    private static final String DATA_TYPE = "DataType";
 
 
     public void updateLevel(UserLevelReq userLevelReq) {
@@ -35,10 +50,31 @@ public class UserLevelAssemService {
         if (!memberConditionVoService.updateLevel(userLevelReq)) {
             logger.warn("update user level failed, param: " + JSON.toJSONString(userLevelReq));
         }
+        
         if (!userInfoService.updateLevel(userLevelReq.getUserId(), userLevelReq.getLevelId())) {
         	logger.warn("update user_info level failed, param: " + JSON.toJSONString(userLevelReq));
+        	//处理失败，则延时1秒把消息放回KAFKA
+			resendMsg(userLevelReq);
         }
     }
+    
+    /**
+     * 处理失败，则延时1秒把消息放回KAFKA
+     * @param userLevelReq
+     */
+	private void resendMsg(UserLevelReq userLevelReq) {
+		Timer timer = new Timer();// 实例化Timer类
+		timer.schedule(new TimerTask() {
+			public void run() {
+				// 处理失败，则延时1秒把消息放回KAFKA
+				JSONObject jsonObject = new JSONObject();
+				jsonObject.put(DATA_TYPE, KafkaConf.DataType.UPDATE_USER_LEVEL.type());
+				jsonObject.put(DATA, userLevelReq);
+				template.send(TOPIC, jsonObject.toJSONString());
+				this.cancel();
+			}
+		}, 1000);// 1秒
+	}
 
     /**
      * 批量修改一段时间内的会员层级
