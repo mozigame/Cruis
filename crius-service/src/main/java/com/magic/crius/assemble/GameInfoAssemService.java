41a4a491abbd34b4620f54a7cccb86064b13f190
justin <justins1707@gmail.com>
2017-07-29 10:51:57 +0800

    log

--- crius-service/src/main/java/com/magic/crius/assemble/GameInfoAssemService.java
+++ crius-service/src/main/java/com/magic/crius/assemble/GameInfoAssemService.java
@@ -74,7 +74,7 @@ public class GameInfoAssemService {
 	                    if (!gameInfoService.deleteAll()) {
 	                        logger.warn("delete gameInfos failed");
 	                    }
-	                    if (!batchSaveGame(gameInfos)) {
+	                    if (!gameInfoService.batchSave(gameInfos)) {
 	                        logger.warn("batchSave gameInfos failed");
 	                    }
                 	}
